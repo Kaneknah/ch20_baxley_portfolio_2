484e11b0fc6218fdd6ea9ac8d2094a6ca2867a9a
Kaneknah <jamesnbaxley@protonmail.com>
2023-10-01 13:07:46 -0600

    about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,25 @@ import React from "react";
 export default function About() {
 	return (
 		<div>
-			<h1>Home Page</h1>
+			<h1>About Page</h1>
 			<p>
-				Coming Soon
+				{" "}
+				I am a former High School teacher who has recently transitioned to web
+				design. Born and raised in San Diego, I attended San Diego State
+				University for my BA in History. I also acquired a Masters in Education
+				from Azusa Pacific University and a Clear Teaching Credential from
+				University of California San Diego. I have been in the Educational Field
+				in California and Montana for over 12 years, before I decided to change
+				directions and pursue a career in web design. I have
+				completed the Berkely Bootcamp in Full Stack Web Development in April
+				and have provided several examples of my completed projects through that
+				course. I have also provided some of my own projects that I have
+				completed on my own. I currently live outside of Missoula Montana with
+				my beautiful wife and my three daughters and son. I am very excited to
+				combined my professional knowledge with my newly learned skills to dive
+				into an engaging career in web design{" "}
 			</p>
+			<br></br>
 		</div>
 	);
 }
